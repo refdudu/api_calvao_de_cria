0e509429b77d611d57b107fad86c0e8a730699fd
Renan Fischer <renan.fischer.2013@gmail.com>
2025-12-06 00:06:24 -0300

    delete JS files

--- src/routes/product.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const productController = require('../controllers/product.controller');
-const { listProductsRules, validate } = require('../utils/validators/product.validator');
-const router = express.Router();
-
-router.get('/', listProductsRules(), validate, productController.getAllProducts);
-router.get('/:productId', productController.getOneProduct);
-
-module.exports = router;
